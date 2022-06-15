6feb4c0eb994e0d8e190fe4d01aa708cec1ad63c
khalil ben hadj <khalilbenhadj2005@gmail.com>
2022-06-15 17:11:25 +0100

    Complete 04_REMOVEFROMARRAY

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    for (const Arg of theArgs) {
+        array=array.filter((item)=> item!==Arg)
+    }
+    return array;
 };
 
 // Do not edit below this line
