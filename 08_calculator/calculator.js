a55bacc08c23c5fdcaa53654a079a3cf82dae078
khalil ben hadj <khalilbenhadj2005@gmail.com>
2022-06-15 19:45:49 +0100

    Complete 08_CALCULATOR

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return(a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result=0;
+  for (item of arr) {
+    result+=item;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result=1;
+  for (item of arr) {
+    result*=item;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return(a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else { 
+      return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
