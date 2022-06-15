7f8c166301007513a369e91c2dc5f0ddddc0e941
khalil ben hadj <khalilbenhadj2005@gmail.com>
2022-06-15 21:09:53 +0100

    Complete 09_PALINDROMES

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string=string.toLowerCase();
+    let text=string;
+    text=text.split("");
+    text=text.filter((value) => ((value!==".")&&(value!==",")&&(value!=="!")&&(value!==" ")))
+    let temp=text;
+    temp=temp.join("")
+    let revText=text;
+    revText.reverse();
+    revText=revText.join("");
+    if (revText===temp) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
