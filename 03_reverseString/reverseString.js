be35ba096f2166897e39d94d36f210209969ae47
khalil ben hadj <khalilbenhadj2005@gmail.com>
2022-06-15 16:32:31 +0100

    Complete 03_REVERSESTRING

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text=string.split("");
+    text=text.reverse();
+    text=text.join("");
+    return text;
 };
 
 // Do not edit below this line
