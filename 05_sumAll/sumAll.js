71a7b1f22e2546a78f59016463d1afd89effc161
khalil ben hadj <khalilbenhadj2005@gmail.com>
2022-06-15 17:36:30 +0100

    Complete 05_SUMALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum=0;
+    if (a>b) {
+        let temp=b;
+        b=a;
+        a=temp;
+    }
+    if (a<0||b<0||typeof(a)!="number"||typeof(b)!="number") {
+        return ("ERROR") ;
+    }
+    for (let i=a;i<=b;i++) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
